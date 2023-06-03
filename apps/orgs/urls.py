8d1c2ec7be66dd8d41337bbe0c80fb3027dc61c1
镜像 <68786940+luo-zhimin@users.noreply.github.com>
2023-06-03 23:01:59 +0800

    add org list

--- apps/orgs/urls.py
+++ apps/orgs/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path, include
+from django.urls import path
+
+from .views import org_list
 
 urlpatterns = [
-    # path(r'^orgs/', include(('orgs.urls', 'orgs'), namespace='orgs')),
+    path('org_list/', org_list, name='org_list'),
 ]
--- apps/orgs/views.py
+++ apps/orgs/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
+from .models import OrgInfo, CityInfo
+
+
 # Create your views here.
+def org_list(request):
+    all_orgs = OrgInfo.objects.all()
+    all_cites = CityInfo.objects.all()
+
+    sort_orgs = all_orgs.order_by('-love_num')[:3]
+
+    return render(request, 'org-list.html', {
+        'all_orgs': all_orgs,
+        'all_cites': all_cites,
+        'sort_orgs': sort_orgs
+    })
