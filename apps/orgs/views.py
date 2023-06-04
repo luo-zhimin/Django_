7e142e32a735b7fb3aa4fac215608f663221fec1
镜像 <68786940+luo-zhimin@users.noreply.github.com>
2023-06-04 17:19:38 +0800

    org page

--- apps/orgs/views.py
+++ apps/orgs/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.shortcuts import render
 
 from .models import OrgInfo, CityInfo
@@ -7,11 +8,22 @@ from .models import OrgInfo, CityInfo
 def org_list(request):
     all_orgs = OrgInfo.objects.all()
     all_cites = CityInfo.objects.all()
-
+    # 倒叙 切分3
     sort_orgs = all_orgs.order_by('-love_num')[:3]
+
+    # 分页
+    pagenum = request.GET.get('pagenum', '')
+    pa = Paginator(all_orgs, 3)
+    try:
+        pages = pa.page(pagenum)
+    except PageNotAnInteger:
+        pages = pa.page(1)
+    except EmptyPage:
+        pages = pa.page(pa.num_pages)
 
     return render(request, 'org-list.html', {
         'all_orgs': all_orgs,
+        'pages': pages,
         'all_cites': all_cites,
         'sort_orgs': sort_orgs
     })
