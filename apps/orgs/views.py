490ded8a61da0874504a160cc866e88f6bd2bca2
镜像 <68786940+luo-zhimin@users.noreply.github.com>
2023-06-04 22:37:39 +0800

    org list and filter

--- apps/orgs/views.py
+++ apps/orgs/views.py
@@ -10,6 +10,20 @@ def org_list(request):
     all_cites = CityInfo.objects.all()
     # 倒叙 切分3
     sort_orgs = all_orgs.order_by('-love_num')[:3]
+    # 按照机构类别进行过滤
+    category = request.GET.get('category', '')
+    if category:
+        all_orgs = all_orgs.filter(category=category)
+    city_id = request.GET.get('city_id', '')
+    if city_id:
+        all_orgs = all_orgs.filter(city_info_id=city_id)
+
+    # 排序
+    sort = request.GET.get('sort', '')
+    if sort:
+        # print(fr'-{sort}')
+        # todo 重新赋值
+        all_orgs = all_orgs.order_by(fr'-{sort}')
 
     # 分页
     pagenum = request.GET.get('pagenum', '')
@@ -25,5 +39,8 @@ def org_list(request):
         'all_orgs': all_orgs,
         'pages': pages,
         'all_cites': all_cites,
-        'sort_orgs': sort_orgs
+        'sort_orgs': sort_orgs,
+        'category': category,
+        'city_id': city_id,
+        'sort': sort
     })
