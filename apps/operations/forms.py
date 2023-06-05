24e3893dc97a13ed6b9ebb54a0fd5bea5f3680e8
镜像 <68786940+luo-zhimin@users.noreply.github.com>
2023-06-05 17:09:09 +0800

    user operation ask

---
+++ apps/operations/forms.py
@@ -0,0 +1,27 @@
+import re
+
+from django import forms
+
+from .models import UserAskInfo
+
+
+class UserAskForm(forms.ModelForm):
+    class Meta:
+        model = UserAskInfo
+        # 排除
+        # exclude=['add_time']
+        # 全部
+        # fields = ['__all__']
+        fields = ['name', 'course', 'phone']
+
+    def clean_phone(self):
+        phone = self.cleaned_data['phone']
+        # 正则
+        co = re.compile('^1[3|4|5|6|7|8|9]\\d{9}$')
+        if co.match(phone):
+            return phone
+        else:
+            raise forms.ValidationError('手机号码不合法')
+
+    # def clean_course(self):
+    # 可以校验是否有该课程
--- apps/operations/views.py
+++ apps/operations/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+
+from .forms import UserAskForm
+
 
 # Create your views here.
+
+def user_ask(request):
+    user_ask_form = UserAskForm(request.POST)
+    if user_ask_form.is_valid():
+        # name = user_ask_form.cleaned_data['name']
+        # 自动提交
+        user_ask_form.save(commit=True)
+        return JsonResponse({'status': 200, 'msg': '咨询成功'})
+    else:
+        return JsonResponse({'status': 500, 'msg': '咨询失败'})
